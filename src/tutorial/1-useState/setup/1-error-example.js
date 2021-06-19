a617ad9fa0fd6a2eb8da2747e210ad4f38c2010d
deekshith-hari <deekshith@musubi-management.com>
2021-06-19 06:16:57 +0530

    first commit

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [title, setTitle] = useState("Hello Deekshith");
+  const handleClick = () => {
+    if (title === "Hello Deekshith") {
+      setTitle("Welcome to canada");
+    } else {
+      setTitle("Hello Deekshith");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({ name: "deekshith", age: "26", message: "random message" });
+  const clickHandler = () => {
+    setPerson({ ...person, message: "Welcome to canada" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={clickHandler}>
+        Click
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
