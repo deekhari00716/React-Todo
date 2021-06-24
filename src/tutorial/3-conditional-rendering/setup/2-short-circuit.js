f26c8f4958886bf1daf9ec2d31b76df930943e4f
deekshith-hari <deekshith@musubi-management.com>
2021-06-24 22:42:11 +0530

    short circuit

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
+  const [text, setText] = useState("Messi");
+  const firstValue = text || "hello world";
+  const seconcValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h1>{text || "Deekshith"}</h1>
+      {text && <h1>Hello world</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
